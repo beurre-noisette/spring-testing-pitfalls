bb56180135f26737a65a0455f968435aab12d172
sangwon <tjtkddnjs857@naver.com>
2025-11-04 22:32:40 +0900

    feat: UserRepository 테스트 추가

---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,17 @@
+insert into users (
+                     id,
+                     email,
+                     nickname,
+                     address,
+                     certification_code,
+                     status,
+                     last_login_at
+) values (
+        1,
+        'test@gmail.com',
+        'james',
+        'Seoul',
+        'aaaaaaa-aaaa-aaaa-aaaaaaaaaa',
+        'ACTIVE',
+        0
+    );
