70996e9444c80054a0e396a56aaa4c909a5f0adb
Sangwon Seo <tjtkddnjs857@naver.com>
2025-11-11 10:51:52 +0900

    feat: userController 테스트코드 추가

---
+++ src/test/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,37 @@
+insert into users (
+                     id,
+                     email,
+                     nickname,
+                     address,
+                     certification_code,
+                     status,
+                     last_login_at
+) values (
+        1,
+        'test@gmail.com',
+        'james',
+        'Seoul',
+        'aaaaaaa-aaaa-aaaa-aaaaaaaaaa',
+        'ACTIVE',
+        0
+    );
+
+insert into users (
+    id,
+    email,
+    nickname,
+    address,
+    certification_code,
+    status,
+    last_login_at
+) values (
+             2,
+             'test2@gmail.com',
+             'tom',
+             'Seoul',
+             'aaaaaaa-aaaa-aaaa-aaaaaaaaab',
+             'PENDING',
+             0
+         );
+
+ALTER TABLE users ALTER COLUMN id RESTART WITH 3;
