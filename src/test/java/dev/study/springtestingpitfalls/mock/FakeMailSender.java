87a883a2c630433abdf9a42a5fd2403fd345bddf
Sangwon Seo <tjtkddnjs857@naver.com>
2025-11-11 14:22:27 +0900

    feat: CertificationService 테스트 추가

---
+++ src/test/java/dev/study/springtestingpitfalls/mock/FakeMailSender.java
@@ -0,0 +1,21 @@
+package dev.study.springtestingpitfalls.mock;
+
+import dev.study.springtestingpitfalls.user.service.port.MailSender;
+
+public class FakeMailSender implements MailSender {
+
+    public String email;
+    public String title;
+    public String content;
+
+    @Override
+    public void send(
+            String email,
+            String title,
+            String content
+    ) {
+        this.email = email;
+        this.title = title;
+        this.content = content;
+    }
+}
---
+++ src/test/java/dev/study/springtestingpitfalls/user/service/CertificationServiceTest.java
@@ -0,0 +1,28 @@
+package dev.study.springtestingpitfalls.user.service;
+
+import dev.study.springtestingpitfalls.mock.FakeMailSender;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CertificationServiceTest {
+
+    @Test
+    void 이메일과_컨텐츠가_제대로_만들어져서_보내는지_테스트한다() {
+        // given
+        FakeMailSender fakeMailSender = new FakeMailSender();
+        CertificationService certificationService = new CertificationService(fakeMailSender);
+
+        // when
+        certificationService.send(
+                "test@gmail.com",
+                1,
+                "aaaaaaa-aaaa-aaaa-aaaaaaaaaa"
+        );
+
+        // then
+        assertThat(fakeMailSender.email).isEqualTo("test@gmail.com");
+        assertThat(fakeMailSender.title).isEqualTo("Please certify your email address");
+        assertThat(fakeMailSender.content).isEqualTo("Please click the following link to certify your email address: http://localhost:8080/api/users/1/verify?certificationCode=aaaaaaa-aaaa-aaaa-aaaaaaaaaa");
+    }
+}
